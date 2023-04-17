1d1a84368a59f39acbba60707e5920d3df9f5d02
shamsulhuda <shamsulhuda310@gmail.com>
2023-04-17 17:00:35 +0600

    batteryEffect

--- src/BatteryStatus.jsx
+++ src/BatteryStatus.jsx
@@ -2,18 +2,35 @@ import { useEffect, useState } from "react";
 
 export default function BatteryStatus() {
   const [isCharging, setIsCharging] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
-    navigator.getBattery().then((battery) => {
-      const onBatteryChange = () => setIsCharging(battery.charging);
-      onBatteryChange();
-      battery.addEventListener("chargingchange", onBatteryChange);
-      battery.removeEventListener("chargingchange", onBatteryChange);
-    });
+    let cleanup;
+    navigator
+      .getBattery()
+      .then((battery) => {
+        const onBatteryChange = () => setIsCharging(battery.charging);
+        onBatteryChange();
+        battery.addEventListener("chargingchange", onBatteryChange);
+        cleanup = () =>
+          battery.removeEventListener("chargingchange", onBatteryChange);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(true);
+      });
+    return () => {
+      // cleanup
+      cleanup?.();
+    };
   }, []);
   return (
     <div>
       <h2>Battery status</h2>
-      <p>Charging: {isCharging ? "YES" : "No"}</p>
+      {error ? (
+        <p>Could not get battery status</p>
+      ) : (
+        <p>Battery is {isCharging ? "Charging" : "Not charging"}</p>
+      )}
     </div>
   );
 }
